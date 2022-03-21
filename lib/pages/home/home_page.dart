13e0c819e9fd03245450e84dc572bc7992db3f22
yonialemye <yonialemye@gmail.com>
2022-03-21 07:50:03 +0300

    build a bottom navigation bar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -32,15 +32,62 @@ class _HomePageState extends State<HomePage> {
               child: Image.asset('images/bg_liquid.png', width: 160),
             ),
           ),
-          Column(
-            children: const [
-              Header(),
-              Search(),
-              Genre(),
-            ],
+          SingleChildScrollView(
+            child: Column(
+              children: const [
+                Header(),
+                Search(),
+                Genre(),
+              ],
+            ),
           )
         ],
       ),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  _buildBottomNavigationBar() {
+    return Container(
+      color: const Color(0xFFF6F8FF),
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.4),
+              spreadRadius: 5,
+              blurRadius: 10,
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topRight: Radius.circular(15),
+            topLeft: Radius.circular(15),
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.white,
+            selectedItemColor: const Color(0xFF5F67EA),
+            selectedFontSize: 12,
+            unselectedFontSize: 12,
+            unselectedItemColor: Colors.grey.withOpacity(0.9),
+            type: BottomNavigationBarType.fixed,
+            items: [
+              _buildNavigationItem('Home', Icons.home),
+              _buildNavigationItem('Application', Icons.more_horiz_outlined),
+              _buildNavigationItem('Videos', Icons.play_arrow_rounded),
+              _buildNavigationItem('Books', Icons.auto_stories_outlined),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildNavigationItem(String title, IconData icon) {
+    return BottomNavigationBarItem(
+      icon: Icon(icon),
+      label: title,
     );
   }
 }
